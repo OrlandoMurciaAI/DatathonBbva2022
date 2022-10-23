ce88ae76594551b27a614a558860699036c30754
xjox360 <jhongomez360@gmail.com>
2022-10-23 02:37:45 -0500

    debugging

--- backend/data_collect.py
+++ backend/data_collect.py
@@ -9,4 +9,4 @@ def building_dataframe(response: dict, s3)->pd.DataFrame:
     print('Recogiendo los datos y enviandolos a s3')
     df = pd.DataFrame(data=response)
     df.to_csv('resultados.csv',sep=',',index=None) 
-    s3.upload_file(local_file='resultados.csv',bucket='stage-buck',s3_file='stage_files/stage_resultados.csv')
+    s3.upload_file('resultados.csv','stage-buck','stage_files/stage_resultados.csv')
